63a5513925a4bc3bbf2f669d9a712c417c53122a
Peyton11 <peyton.gardner03@gmail.com>
2024-11-20 19:09:33 -0500

    Create cylinder

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.m
 
 import { createCube } from "./shapes/cube.js";
 import { createSphere } from "./shapes/sphere.js";
+import { createCylinder } from "./shapes/cylinder.js";
 import { setAllFalse, enablePhongShading, disablePhongShading } from "./utils.js";
 
 // Create the scene
@@ -42,11 +43,12 @@ document.body.appendChild(renderer.domElement);
 // Create each shape
 let cube = createCube();
 let sphere = createSphere();
+let cylinder = createCylinder();
 
+// Only one shape is allowed at once
 let activeShape = cube;
 scene.add(activeShape);
 renderer.render(scene, camera);
-
 
 // Flags to control states
 let spinningForward = false;
---
+++ src/shapes/cylinder.js
@@ -0,0 +1,15 @@
+// Peyton Gardner
+// src/shapes/cylinder.js
+
+// Uncomment the following line for local development:
+// import * as THREE from 'three';
+
+// Uncomment the following line for production. This is required to view in browsers:
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.module.js';
+
+export function createCylinder() {
+    let geometry = new THREE.CylinderGeometry(1, 1, 2);
+    let material = new THREE.MeshStandardMaterial({color: 0x808080});
+    let cylinder = new THREE.Mesh(geometry, material);
+    return cylinder;
+}
